cb9c335e27c67cd408630a7eafc9ea42104b0682
Keshav Jangid 009 <149224434+keshavrajora@users.noreply.github.com>
2024-10-07 22:20:12 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+
+    <?php
+for ($i = 1; $i <= 10; $i++) { 
+    for ($b = 1; $b <= 10; $b++) { 
+        echo "$i * $b = " . ($i * $b) . "<br>" ,"<br>"; 
+    }
+}
+
+
+?>
+</body>
+</html>
